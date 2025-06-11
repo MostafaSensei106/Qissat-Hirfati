f801ee0ae1b099a5d91260af99dc1d3ea9f61d77
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-06-11 22:50:14 +0300

    Refactor code structure for improved readability and maintainability

--- lib/core/config/theme/colors/light_theme.dart
+++ lib/core/config/theme/colors/light_theme.dart
@@ -3,10 +3,33 @@ import 'package:flutter/cupertino.dart';
 final CupertinoThemeData lightTheme = CupertinoThemeData(
   brightness: Brightness.light,
   primaryColor: const Color(0xFF9D653B),
-  primaryContrastingColor: const Color(0xFF9D653B),
   barBackgroundColor: const Color(0xFFFFFFFF),
   scaffoldBackgroundColor: const Color(0xFFFFFFFF),
   applyThemeToAll: true,
 
-  
+  textTheme: CupertinoTextThemeData(
+    primaryColor: const Color(0xFF9D653B),
+    textStyle: TextStyle(
+      color: const Color(0xFF333333),
+      fontSize: 16,
+      fontFamily: 'SF Pro', 
+    ),
+    navTitleTextStyle: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: const Color(0xFF333333),
+      fontFamily: 'SF Pro',
+    ),
+    navLargeTitleTextStyle: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+      color: const Color(0xFF333333),
+      fontFamily: 'SF Pro',
+    ),
+    actionTextStyle: TextStyle(
+      color: const Color(0xFF9D653B),
+      fontSize: 16,
+      fontFamily: 'SF Pro',
+    ),
+  ),
 );
