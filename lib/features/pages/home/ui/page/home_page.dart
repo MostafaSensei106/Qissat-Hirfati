020849e5c576cb85da69a8275d6cd75eecfa31b5
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-06-12 17:14:22 +0300

    feat: Enhance HomePage layout and UI components for better user experience

--- lib/features/pages/home/ui/page/home_page.dart
+++ lib/features/pages/home/ui/page/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qissat_hirfati/core/config/const/app_const.dart';
 import 'package:qissat_hirfati/core/widgets/cupertino_buttons_component/cupertino_button_component/cupertino_button_component.dart';
+import 'package:qissat_hirfati/core/widgets/cupertino_buttons_component/cupertino_button_filled_component/cupertino_button_filled_component.dart';
 import 'package:qissat_hirfati/features/pages/settings/ui/page/settings_page.dart';
 import 'package:qissat_hirfati/l10n/app_localizations.dart';
 
@@ -26,12 +28,9 @@ class HomePage extends StatelessWidget {
       ),
 
       child: SingleChildScrollView(
+        physics: const ScrollPhysics(),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Landing(),
-          TextSectionCom()
-          ],
+          children: [Landing(), TextSectionCom()],
         ),
       ),
     );
@@ -39,15 +38,47 @@ class HomePage extends StatelessWidget {
 }
 
 class TextSectionCom extends StatelessWidget {
-  const TextSectionCom({
-    super.key,
-  });
+  const TextSectionCom({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
+      clipBehavior: Clip.none,
       children: [
-        Image.asset(AppConstants.smallTreePNG, fit: BoxFit.cover),
+        Container(
+          padding: EdgeInsets.all(AppConstants.padding),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'أيادٍ سعودية مبدعة،\nتنسج المستقبل بلمسات مستوحاة من التراث وعراقة الأصالة.',
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: 0.90.sw,
+                child: Text(
+                  'نسعى للارتقاء بمستقبل الحرف اليدوية السعودية، لتقديم إبداعات عصرية تنبض بروح التراث الأصيل.',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 35),
+              CupertinoButtonFilledComponent(onPressed: () {}, text: 'استكشف المزيد', ),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 0,
+          top: 0,
+          bottom: 0,
+          child: Image.asset(
+            AppConstants.smallTreePNG,
+            fit: BoxFit.contain,
+            filterQuality: FilterQuality.high,
+            opacity: const AlwaysStoppedAnimation(.5),
+          ),
+        ),
       ],
     );
   }
@@ -60,7 +91,12 @@ class Landing extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.center,
-      children: [Image.asset(AppConstants.landHomeGif, fit: BoxFit.cover)],
+      children: [Image.asset(AppConstants.landHomeGif, fit: BoxFit.cover ,),
+        Text(
+          'أيدٍ سعودية مبدعة.',
+          style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold ,color: CupertinoColors.white),
+        ),
+      ],
     );
   }
 }
