44e2e43fb875c52ad81ca92c1992af9ec4705456
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-07-10 19:02:22 +0300

    feat(locale): Persist selected language and update translations

--- lib/core/locals/logic/cubit/local_cubit.dart
+++ lib/core/locals/logic/cubit/local_cubit.dart
@@ -1,11 +1,27 @@
 // lib/cubit/locale_cubit.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleCubit extends Cubit<Locale> {
-  LocaleCubit() : super(const Locale('ar', 'SA'));
 
-  void changeLocale(Locale locale) {
+  LocaleCubit() : super(const Locale('ar', 'SA')) {
+    _loadSavedLocale();
+  }
+  static const String _localeKey = 'saved_locale';
+
+  Future<void> _loadSavedLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final code = prefs.getString(_localeKey);
+    if (code != null) {
+      final parts = code.split('_');
+      emit(Locale(parts[0], parts.length > 1 ? parts[1] : null));
+    }
+  }
+
+  Future<void> changeLocale(Locale locale) async {
     emit(locale);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_localeKey, '${locale.languageCode}_${locale.countryCode ?? ''}');
   }
 }
